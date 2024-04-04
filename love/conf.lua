e1d491f557a75919b02efc16fc8f984ce5bbc4b6
notoroy <pasaroy2@gmail.com>
2024-04-04 20:27:03 +0300

    lua.libraries

--- love/conf.lua
+++ love/conf.lua
@@ -1,4 +1,56 @@
-function love.conf(t)
-    t.modules.timer = false
+function love.run()
+
+	if love.math then
+		love.math.setRandomSeed(os.time())
+	end
+
+	if love.load then love.load(arg) end
+
+	-- We don't want the first frame's dt to include time taken by love.load.
+	if love.timer then love.timer.step() end
+
+	local dt = 0
+    upper_dt = 1 / 60
+
+	-- Main loop time.
+	while true do
+		-- Process events.
+		if love.event then
+			love.event.pump()
+			for name, a,b,c,d,e,f in love.event.poll() do
+				if name == "quit" then
+					if not love.quit or not love.quit() then
+						return a
+					end
+				end
+				love.handlers[name](a,b,c,d,e,f)
+			end
+		end
+
+		-- Update dt, as we'll be passing it to update
+		if love.timer then
+			love.timer.step()
+			dt = love.timer.getDelta()
+		end
+
+        while dt > 0 do
+            local current_dt = math.min(dt, upper_dt)
+            if love.update then love.update(current_dt) end
+            dt = dt - current_dt
+        end
+
+		-- Call update and draw
+		if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
+
+		if love.graphics and love.graphics.isActive() then
+			love.graphics.clear(love.graphics.getBackgroundColor())
+			love.graphics.origin()
+			if love.draw then love.draw() end
+			love.graphics.present()
+		end
+
+         
+		if love.timer then love.timer.sleep(0.001) end
+	end
 
 end
--- love/main.lua
+++ love/main.lua
@@ -1,11 +1,34 @@
+Object = require 'libraries/classic/classic'
+require 'objects/test'
+
 function love.load()
-    image = love.graphics.newImage("images/wrench.jpeg")
+    local object_files = {}
+    recursiveEnumerate('objects', object_files)
+end
+
+function recursiveEnumerate(folder, file_list)
+    local items = love.filesystem.getDirectoryItems(folder)
+    for _, item in ipairs(items) do
+        local file = folder .. '/' .. item
+        if love.filesystem.isFile(file) then
+            table.insert(file_list, file)
+        elseif love.filesystem.isDirectory(file) then
+            recursiveEnumerate(file, file_list)
+        end
+    end
+end
+
+function requireFiles(files)
+    for _, file in ipairs(files) do
+        local file = file:sub(1, -5)
+        require(file)
+    end
 end
 
 function love.update(dt)
-
+    frame = frame + 1
 end
 
 function love.draw()
-    love.graphics.draw(image, love.math.random(0,800), love.math.random(0,600))
+    print(frame)
 end
